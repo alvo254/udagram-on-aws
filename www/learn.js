0fa5b5b06389b6f70bbe52bb8576e718332c26f5
alvo254 <41018786+alvo254@users.noreply.github.com>
2022-08-13 15:06:44 +0300

    Delete www directory

--- www/learn.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const cost = 2;
-console.log(cost);
-const james = {
-    name: 'adrian',
-    age: 22
-};
-console.log(james.name + ' ' + james.age);
-//# sourceMappingURL=learn.js.map
